9f9cc1d87d472918bd2a23d5486710d205f5b103
shingo kubomura <skksb75@gmail.com>
2022-06-20 15:02:59 +0900

    update index.jsx

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,28 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
+import App from "./components/App";
+
+// functional component
+// const Hello = (props) => {
+//   const { name, age } = props;
+//   return <h1>Hi I'm { name }, { age } years old</h1>;
+// };
+
+// Class component
+// class Hello extends React.Component {
+//   render() {
+//     const { name, age } = this.props;
+//     return <h1>Hi I'm { name }, { age } years old</h1>;
+//   }
+// }
+
+const container = document.querySelector("#root");
+
+if (container) {
+  const root = ReactDOM.createRoot(container);
+
+  root.render(<App />);
+}
